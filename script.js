09cacabf92b3e8a95111d314861ffe1718e49a0b
Your Name <you@example.com>
2021-01-18 15:10:20 +0600

    let starring hello

---
+++ script.js
@@ -0,0 +1,55 @@
+// let staring1 = "Hello";
+// let staring2 = "World";
+// let staring3 = "!!!";
+// let staring4 = "123";
+
+
+// let number2 = 15_000_000_000;
+// let number3 = 1.5e10;
+// let number4 = 0.0000000001;
+// let number5 = 1e-10;
+// let number6 = 0xff;
+// let number7 = NaN;
+// let number8 = Infinity;
+
+// let number1 = true;
+// let number2 = false;
+
+
+
+// let person = {
+//     firstName: "Eleman"
+//     lastName: "Imashov"
+// };
+
+// console.log(person);
+
+
+let math1 = 1 + 1;
+let math2 = 1 - 1;
+let math3 = 1 * 1;
+let math4 = 1 / 1;
+let math5 = 5 ** 2;
+let math6 = 6 ** 5;
+let math7 = 9 **(1/2);
+let math8 = 10 % 4;
+let math9 = 10 % 12;
+
+// increment
+let example1 = 1;
+example1 =example1 + 1;
+example1 += 1;
+example1++;
+
+
+example1 = example1 / 2;
+example1 = example1 /= 2;
+
+
+example1 = example1 * 2;
+example1 *= 2;
+
+
+let example2 = -1;
+let example3 = +1;
+
